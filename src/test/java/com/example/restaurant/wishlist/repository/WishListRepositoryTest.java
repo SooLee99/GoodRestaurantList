ccf74fb974392d81d68a9f81b76b6a0ffdc8aeee
LeeSooo <dltn990930@naver.com>
2023-03-13 11:12:15 +0900

    docs: Memory CRUD DB 정리 (2023-03-13)

--- src/test/java/com/example/restaurant/wishlist/repository/WishListRepositoryTest.java
+++ src/test/java/com/example/restaurant/wishlist/repository/WishListRepositoryTest.java
@@ -16,6 +16,7 @@ public class WishListRepositoryTest {
     @Autowired
     private WishListRepository wishListRepository;
 
+    // 테스트 수행 동작 시, 객체를 생성하는 메소드.
     private WishListEntity create() {
         var wishList = new WishListEntity();
         wishList.setTitle("title");
@@ -31,57 +32,83 @@ public class WishListRepositoryTest {
         return wishList;
     }
 
+    // saveTest() : 데이터가 재대로 저장되는지 확인하는 메소드.
     @Test
     public void saveTest(){
         var wishListEnity = create();
+        // 데이터 1개를 저장함. -> (총 데이터의 개수 : 1개)
         var expected = wishListRepository.save(wishListEnity);
 
+        // Assertions : 인수를 검증하고 조건에 맞지 않는 경우 IllegalArgumentException 또는 IllegalStateException를 발생시킴.
+        // Assertions : 테스트가 원하는 결과를 제대로 리턴하는지 에러는 발생하지 않는지 확인할 때 사용하는 메소드.
+
+        // 데이터를 리턴했을 때, 데이터의 개수가 null이면 안된다. -> (총 데이터의 개수 : 1개)
         Assertions.assertNotNull(expected);
+        // 첫 번째 인덱스의 데이터가 존재하면, 데이터 저장 성공.
         Assertions.assertEquals(1, expected.getIndex());
     }
 
+    // updateTest() : 데이터 수정 시, 수정이 재대로 되는지 확인하는 메소드.
     @Test
     public void updateTest(){
         var wishListEnity = create();
+        // 데이터 1개를 저장함. -> (총 데이터의 개수 : 1개)
         var expected = wishListRepository.save(wishListEnity);
 
+        // 객체 제목을 "title"에서 "update test"로 변경해서 저장함.
         expected.setTitle("update test");
         var saveEntity = wishListRepository.save(expected);
 
+        // 변경된 데이터가 재대로 저장되어있는지 확인.
         Assertions.assertEquals("update test", saveEntity.getTitle());
+
+        // 데이터를 수정했기 때문에 데이터는 총 1개만 존재해야 한다. 확인되면 테스트 성공.
         Assertions.assertEquals(1, wishListRepository.listAll().size());
     }
 
+    // findByIdTest() : 저장시킨 데이터가 재대로 리턴되는지 확인하는 메소드.
     @Test
     public void findByIdTest(){
         var wishListEnity = create();
+        // 데이터 1개를 저장함. -> (총 데이터의 개수 : 1개)
         wishListRepository.save(wishListEnity);
 
+        // 데이터 1개를 리턴시킴. -> (총 데이터의 개수 : 1개)
         var expected = wishListRepository.findById(1);
 
+        // 객체 안의 값이 존재해야 함.
         Assertions.assertEquals(true, expected.isPresent());
+        // 인덱스의 값을 리턴했을 때, 이 인덱스는 1이어야 함.
         Assertions.assertEquals(1, expected.get().getIndex());
     }
 
+    // deleteTest() : 저장시킨 데이터가 재대로 삭제되는지 확인하는 메소드.
     @Test
     public void deleteTest() {
         var wishListEnity = create();
+        // 데이터 1개를 저장함. -> (총 데이터의 개수 : 1개)
         wishListRepository.save(wishListEnity);
 
+        // 데이터 1개를 삭제함. -> (총 데이터의 개수 : 0개)
         wishListRepository.deleteById(1);
 
+        // 총 데이터의 개수가 0개이면, 삭제 성공.
         int count = wishListRepository.listAll().size();
         Assertions.assertEquals(0, count);
     }
 
+    // listAllTest() : 전체 리스트가 재대로 리턴되는지 확인하는 메소드.
     @Test
     public void listAllTest() {
+        // 객체1 생성.
         var wishListEnity1 = create();
         wishListRepository.save(wishListEnity1);
 
+        // 객체2 생성.
         var wishListEnity2 = create();
         wishListRepository.save(wishListEnity2);
 
+        // 객체 2개가 리턴되면 테스트 성공.
         int count = wishListRepository.listAll().size();
         Assertions.assertEquals(2, count);
     }
